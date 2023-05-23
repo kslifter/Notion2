fa83bfeeed84a425256b8a842adc4b58e28a2950
kslifter <91616483+kslifter@users.noreply.github.com>
2023-05-23 13:46:25 -0500

    Update todays-schedule-email.py

--- dailyEmail/todays-schedule-email.py
+++ dailyEmail/todays-schedule-email.py
@@ -26,20 +26,12 @@ tomorrow = (
 ).astimezone().isoformat()
 query = {
     "filter": {
-        "and": [
             {
                 "property": "Assign",
                 "relation": {
                     "contains": "Ian-Hartsook-05a52cbc85d14e178992b3f5a598b1f4"
+							}
             }
-            },
-            {
-                "property": "Category",
-                "select": {
-                    "is": "Template"
-               }
-           }
-      ]
     },
 	"sorts": [
 		{

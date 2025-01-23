493e33dae4000b63fd76229fe627630890aa6b85
kslifter <91616483+kslifter@users.noreply.github.com>
2025-01-23 13:08:36 -0600

    Update retrieveNotionDatabase.py

--- dailyEmail/service/retrieveNotionDatabase.py
+++ dailyEmail/service/retrieveNotionDatabase.py
@@ -3,27 +3,35 @@ import json
 
 HeadersType = dict[str, str]
 
-
 def retrieveDatabase(
         databaseId: str,
         headers: HeadersType,
         query: dict = {},
         save_to_json: bool = False) -> dict:
 
-    # Construct database url
+    # Construct database URL
     base_url = "https://api.notion.com/v1/databases/"
     url = base_url + databaseId + "/query"
 
     # POST
-    response = requests.post(url=url, headers=headers, json=query)
-    if response.status_code == 200:
-        database = response.json()
+    try:
+        response = requests.post(url=url, headers=headers, json=query)
 
-        if save_to_json:
-            with open("./dailyEmail/saved-data/database.json", "w", encoding="utf8") as f:
-                json.dump(database, f, ensure_ascii=False)
+        # Check for success
+        if response.status_code == 200:
+            database = response.json()
 
-        return database
-    else:
-        print(f"Error {response.status_code} with query: {query}")
+            if save_to_json:
+                with open("./dailyEmail/saved-data/database.json", "w", encoding="utf8") as f:
+                    json.dump(database, f, ensure_ascii=False)
+
+            return database
+        else:
+            # Log error details
+            print(f"Error {response.status_code} with query: {query}")
+            print(f"Response Content: {response.text}")
+            return dict()
+    except requests.exceptions.RequestException as e:
+        # Handle and log exceptions
+        print(f"An error occurred while making the API request: {e}")
         return dict()
